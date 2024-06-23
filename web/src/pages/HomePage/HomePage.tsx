04e115f6f1387260874fd68b67440d30e742b527
moorels <sales@micropls.com>
2024-06-23 17:34:24 +1000

    First

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,18 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+
+      <h1>HomePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
+      </p>
+
+    </>
+  )
+}
+
+export default HomePage
